302d5c8824eaed5f26905fbe9c8883327c4180da
Saud Zahir <mzahir.bee19seecs@seecs.edu.pk>
2023-02-10 22:20:39 +0500

    Add ACS712 arduino code.

--- mux_sensor.ino
+++ mux_sensor.ino
@@ -4,8 +4,15 @@ const int S2 = 10;
 const int S3 = 11;
 const int EN = 12;
 const float adc = (5.0/1023.0);
-const float G = 6.0;
+const float G = 8.0;
+const float R = 12;
 const int N = 16;
+
+
+int S = 66; // Sensitivity
+int adc_val = 0;
+int Vo = 2500;
+double Vadc = 0;
 
 
 void setup() {
@@ -19,8 +26,14 @@ pinMode(EN, OUTPUT);
 
 
 void loop() {
-  float voltage = 0;
+  double voltage = 0;
+  double current = 0;
+  double V = 0;
+  double I = 0;
 
+  adc_val = analogRead(A1);
+  Vadc = (adc_val / 1024.0) * 5000;
+  I = ((Vadc - Vo) / S);
 
 for (int i = 0; i < N; i++) {
   digitalWrite(S0, i & 0b0001);
@@ -28,9 +41,22 @@ for (int i = 0; i < N; i++) {
   digitalWrite(S2, i & 0b0100);
   digitalWrite(S3, i & 0b1000);
   digitalWrite(EN, LOW);  
-  delay(500);
+  delay(1000);
 
   voltage = analogRead(A0)*adc*G;
-  Serial.println(voltage);
+  V += voltage;
+  
+  Serial.print("B");
+  Serial.print(i+1);
+  Serial.print(" Votage: ");
+  Serial.println(voltage, 4);
   }
+  delay(1000);
+  // current = V/R;
+  Serial.print("BAT Current: ");
+  Serial.println(I, 4);
+  // Serial.println(current);
+  Serial.println("");
+  Serial.println("");
+  delay(1000);
 }
